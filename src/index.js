101eec6fee0ae9d7dcbba4a984f01ece146ef85e
Grigoriy Levanov <glevanov@gmail.com>
2019-03-22 20:23:02 +0300

    Render list of mock books

--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import mockBooks from './books';
 
-class Home extends React.Component {
+function BookList(props) {
+    const books = props.books;
+    const noCover = require('./nocover.jpg');
+
+    const listItems = books.map((book) => (
+        <li>
+            <article className="book">
+                <img src={noCover} alt={book.title}/>
+                <p className="book__description book__description--title">{book.title}</p>
+                <p className="book__description book__description--author">{book.author}</p>
+                <p className="book__description">{book.isbn}</p>
+                <p className="book__description">{book.year}</p>
+                <p className="book__description">{book.rating}</p>
+                <p className="book__description">{book.description}</p>
+            </article>
+        </li>
+    ));
+
+    return (
+        <ul>{listItems}</ul>
+    )
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: mockBooks,
+        };
+    }
+
     render() {
         return (
-            <h1>Библиотека</h1>
-        )
+            <>
+                <h1>Библиотека</h1>
+                <BookList books={this.state.books} />
+            </>
+        );
     }
 }
 
 ReactDOM.render(
-    <Home />,
+    <App />,
     document.getElementById('root'),
 );
