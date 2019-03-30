1a69eaa20e3ce82a1c3486f6ac0ef9a6181c2268
Grigoriy Levanov <glevanov@gmail.com>
2019-03-30 12:04:32 +0300

    Update css in buttons

--- src/components/AddControls.js
+++ src/components/AddControls.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function AddControls() {
     return (
         <button
-            className="modal__button"
+            className="book-edit__button"
             type="submit"
         >
             Добавить
--- src/components/InspectControls.js
+++ src/components/InspectControls.js
@@ -4,16 +4,16 @@ import PropTypes from 'prop-types';
 export default class InspectControls extends React.Component {
     render() {
         return (
-            <div className="modal__buttons-wrap">
+            <div className="book-edit__buttons-wrap">
                 <button
-                    className="modal__button"
+                    className="book-edit__button"
                     type="button"
                     onClick={this.props.toggleEditable}
                 >
                     Редактировать книгу
                 </button>
                 <button
-                    className="modal__button"
+                    className="book-edit__button"
                     type="submit"
                     onClick={(evt) => this.props.handleSubmit(evt)}
                     disabled={!this.props.isEditable}
