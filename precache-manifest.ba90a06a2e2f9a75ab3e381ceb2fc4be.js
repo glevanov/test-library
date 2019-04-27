fe39ad9327eb09866286648b3731ccabc31a99b8
Grigoriy Levanov <glevanov@gmail.com>
2019-04-27 10:02:23 +0300

    Updates

---
+++ precache-manifest.ba90a06a2e2f9a75ab3e381ceb2fc4be.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f6652096fad35bb930a2ed1904fc6c4",
+    "url": "/test-library/static/media/roboto-v18-cyrillic_latin-regular.7f665209.woff2"
+  },
+  {
+    "revision": "2b9dfec7807cb4a69fbb276eb62c3d80",
+    "url": "/test-library/static/media/roboto-v18-cyrillic_latin-regular.2b9dfec7.woff"
+  },
+  {
+    "revision": "3125c8cad303341c2702a7df5a2ba397",
+    "url": "/test-library/static/media/roboto-v18-cyrillic_latin-700.3125c8ca.woff"
+  },
+  {
+    "revision": "21f4b44bd7f86b93c6bac09146f7de33",
+    "url": "/test-library/static/media/roboto-v18-cyrillic_latin-700.21f4b44b.woff2"
+  },
+  {
+    "revision": "5218a6ad7069bacf9ff0",
+    "url": "/test-library/static/js/runtime~main.80c2ef26.js"
+  },
+  {
+    "revision": "09108433f32552dfa50c",
+    "url": "/test-library/static/js/main.1bb33208.chunk.js"
+  },
+  {
+    "revision": "0ba018552a6c20fce851",
+    "url": "/test-library/static/js/2.5125ac5d.chunk.js"
+  },
+  {
+    "revision": "09108433f32552dfa50c",
+    "url": "/test-library/static/css/main.1a953c40.chunk.css"
+  },
+  {
+    "revision": "949d879b4673941522a13acd6dfa5f9a",
+    "url": "/test-library/index.html"
+  }
+];
